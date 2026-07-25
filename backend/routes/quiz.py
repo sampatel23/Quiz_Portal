ac229f49e5c2f269b19a836cd5a9b57040bbfaac
Samarth <samarthpatel23104@gmail.com>
2026-07-25 16:17:15 +0530

    added submit API

--- backend/routes/quiz.py
+++ backend/routes/quiz.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from bson.errors import InvalidId
 
 from services.quiz_service import (
     get_all_quizzes,
-    get_quiz_by_id
+    get_quiz_by_id,
+    submit_quiz
 )
 
 quiz_bp = Blueprint("quiz", __name__)
@@ -33,4 +34,17 @@ def get_quiz(quiz_id):
 
         return jsonify({"error": "Invalid Quiz ID"}), 400
     
+@quiz_bp.route("/submit", methods=["POST"])
+def submit():
+    data = request.get_json()
     
+    quiz_id = data.get("quiz_id")
+
+    answers = data.get("answers")
+
+    result = submit_quiz(quiz_id, answers)
+
+    if result is None:
+        return jsonify({"error": "Quiz not found"}), 404
+
+    return jsonify(result) 
